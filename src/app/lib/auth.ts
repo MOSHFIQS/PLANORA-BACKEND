af7638ba40557ec797ac3476fda0ad4deeefeb7d
MOSHFIQS <moshfiqs.dev@gmail.com>
2026-03-17 14:38:14 +0600

    auth file initialized following the User schema to add additional fields

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -2,6 +2,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+import { Role, UserStatus } from "../../generated/prisma/enums"; 
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -12,7 +13,38 @@ export const auth = betterAuth({
     enabled: true, 
   },
 
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        required: true,
+        defaultValue: Role.USER,
+      },
+      status: {
+        type: "string",
+        required: true,
+        defaultValue: UserStatus.ACTIVE,
+      },
+      isDeleted: {
+        type: "boolean",
+        required: true,
+        defaultValue: false,
+      },
+      deletedAt: {
+        type: "date",
+        required: false, // optional
+      },
+    },
+  },
 
+  session: {
+    expiresIn: 60 * 60 * 24, // 1 day in seconds
+    updateAge: 60 * 60 * 24, // 1 day in seconds
+    cookieCache: {
+      enabled: true,
+      maxAge: 60 * 60 * 24, // 1 day
+    },
+  },
 
   // Optional: trusted origins for CORS, especially if you deploy frontend separately
   // trustedOrigins: [process.env.BETTER_AUTH_URL || "http://localhost:3000"],
